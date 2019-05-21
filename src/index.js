498a5588cc9cc4ee43b50501d8d77306f4d57c66
Jeremy Greer <jex.grizzle@gmail.com>
2019-05-20 22:33:18 -0700

    Add fromHash

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 // 3
-const threeWay = (cond1, result1, cond2, result2) => (a) => {
-  if (a === cond1) {
-    return result1
-  } else if (a === cond2) {
-    return result2
+const getParityAddition = (a, b) => {
+  if (bothOdd(a, b)) {
+    return 'even'
+  } else if (eitherOdd(a, b)) {
+    return 'odd'
   }
+  return 'even'
 }
 
-const mixRed = threeWay('yellow', 'orange', 'blue', 'purple')
+// 3
+const doubleCheck = (check1, cb1, check2, cb2) => (a, b) => {
+  if (check1(a, b)) {
+    return cb1(a, b)
+  } else if (check2(a, b)) {
+    return cb2(a, b)
+  }
+}
 
 // 3
 const sendArg = (fn, value) => (a, b) => {
@@ -18,14 +26,12 @@ const sendArg = (fn, value) => (a, b) => {
   }
 }
 
-// 3
-const threeWayCheck = (check1, cb1, check2, cb2) => (a, b) => {
-  if (check1(a, b)) {
-    return cb1(a, b)
-  } else if (check2(a, b)) {
-    return cb2(a, b)
-  }
-}
+const fromHash = (hash) => (key) => hash[key]
+
+const twoWay = (cond1, result1, cond2, result2) => fromHash({
+  [cond1]: result1,
+  [cond2]: result2,
+})
 
 // 3
 export const mixDirections = (a, b) => {
@@ -46,10 +52,11 @@ const has = (value) => (a, b) => a === value || b === value
 const hasRed = has('red')
 const hasBlue = has('blue')
 
+const mixRed = twoWay('yellow', 'orange', 'blue', 'purple')
 const mixRedOnSomeSide = sendArg(mixRed, 'red')
 const mixBlueOnSomeSide = sendArg(mixBlue, 'blue')
 
-export const mixColors = threeWayCheck(hasRed, mixRedOnSomeSide, hasBlue, mixBlueOnSomeSide)
+export const mixColors = doubleCheck(hasRed, mixRedOnSomeSide, hasBlue, mixBlueOnSomeSide)
 
 const bothX = (x) => (a, b) => (a === x && b === x)
 const eitherX = (x) => (a, b) => (a === x || b === x)
@@ -67,15 +74,6 @@ export const getProductSign = (a, b) => {
   return 'positive'
 }
 
-// 3
-const getParityAddition = (a, b) => {
-  if (bothOdd(a, b)) {
-    return 'even'
-  } else if (eitherOdd(a, b)) {
-    return 'odd'
-  }
-  return 'even'
-}
 
 const getParityMultiplication = (a, b) => (a === b)
   ? a
