a8c8055aaa2e7b7fb1154691b203b58e5563f71a
Jeremy Greer <jex.grizzle@gmail.com>
2019-05-21 08:20:09 -0700

    Add tests

--- src/util.test.js
+++ src/util.test.js
@@ -1,18 +1,12 @@
 import * as mod from './util'
 
-describe('util', () => {
-  it('should have a bunch of functions!', () => {
-    expect(Object.keys(mod).length > 1).toBe(true)
-  })
-})
+const no = () => 0
+const yes = () => 1
+const plus = (a, b) => a + b
+const minus = (a, b) => a - b
+const multiply = (a, b) => a * b
 
 describe('conditionallyCall', () => {
-  const no = () => 0
-  const yes = () => 1
-  const plus = (a, b) => a + b
-  const minus = (a, b) => a - b
-  const multiply = (a, b) => a * b
-
   it('should call the default function', () => {
     const result = mod.conditionallyCall([], plus)(1, 2)
     expect(result).toBe(3)
@@ -32,4 +26,45 @@ describe('conditionallyCall', () => {
     ], minus)(7, 11)
     expect(result).toBe(77)
   })
+})
+
+describe('ternaryDo', () => {
+  it('should call the first function', () => {
+    const result = mod.ternaryDo(
+      yes, minus, plus
+    )(1, 2)
+    expect(result).toBe(-1)
+  })
+
+  it('should call the second function', () => {
+    const result = mod.ternaryDo(
+      no, minus, plus
+    )(1, 2)
+    expect(result).toBe(3)
+  })
+})
+
+describe('ifThen', () => {
+  it('should call the first function', () => {
+    const result = mod.ifThen(
+      yes, minus
+    )(1, 2)
+    expect(result).toBe(-1)
+  })
+
+  it('should return undefined', () => {
+    const result = mod.ifThen(
+      no, minus
+    )(1, 2)
+    expect(result).toBe()
+  })
+})
+
+test.each([
+  [1, 1, 1, true],
+  [1, 0, 1, false],
+  [1, 1, 0, false],
+  [1, 0, 0, false],
+])('both(%d)(%d, %d) === %s', (a, b, c, expected) => {
+  expect(mod.both(a)(b, c)).toBe(expected)
 })
