81aee9c585ffef956916db0268bb9f5c1a2d392c
Jeremy Greer <jex.grizzle@gmail.com>
2019-05-22 21:17:15 -0700

    Remove all Jest

--- src/util.test.js
+++ src/util.test.js
@@ -1,16 +1,5 @@
+import tape from 'tape'
 import * as mod from './util'
-
-const describe = () => {}
-const fdescribe = () => {}
-const xdescribe = () => {}
-const it = () => {}
-const xit = () => {}
-const fit = () => {}
-const test = () => {}
-const each = () => () => {}
-test.each = each
-it.each = each
-describe.each = each
 
 const no = () => 0
 const yes = () => 1
@@ -21,102 +10,114 @@ const throws = () => {
   throw new Error('yoink')
 }
 
-describe('conditionallyCall', () => {
-  it('should call the default function', () => {
-    const result = mod.conditionallyCall([], plus)(1, 2)
-    expect(result).toBe(3)
-  })
+// conditionallyCall
+tape('should call the default function', (t) => {
+  const result = mod.conditionallyCall([], plus)(1, 2)
+  t.equal(3, result)
+  t.end()
+})
 
-  it('should call the first function', () => {
-    const result = mod.conditionallyCall([
-      [yes, minus]
-    ], plus)(1, 2)
-    expect(result).toBe(-1)
-  })
+tape('should call the first function', (t) => {
+  const result = mod.conditionallyCall([
+    [yes, minus]
+  ], plus)(1, 2)
+  t.equal(-1, result)
+  t.end()
+})
 
-  it('should call the second function', () => {
-    const result = mod.conditionallyCall([
-      [no, plus],
-      [yes, multiply],
-    ], minus)(7, 11)
-    expect(result).toBe(77)
-  })
+tape('should call the second function', (t) => {
+  const result = mod.conditionallyCall([
+    [no, plus],
+    [yes, multiply],
+  ], minus)(7, 11)
+  t.equal(77, result)
+  t.end()
+})
 
-  it('should throw', () => {
-    expect(() => {
-      mod.conditionallyCall([
+tape('should throw', (t) => {
+  try {
+    mod.conditionallyCall([
         [throws, yes]
-      ], yes)(1, 2)
-    }).toThrow('yoink')
-  })
+    ], yes)(1, 2)
+  } catch (error) {
+    t.equal('yoink', error.message)
+  }
+  t.end()
 })
 
-describe('doTernary', () => {
-  it('should call the first function', () => {
-    const result = mod.doTernary(
-      yes, minus, plus
-    )(1, 2)
-    expect(result).toBe(-1)
-  })
-
-  it('should call the second function', () => {
-    const result = mod.doTernary(
-      no, minus, plus
-    )(1, 2)
-    expect(result).toBe(3)
-  })
+// doTernary
+tape('should call the first function', (t) => {
+  const result = mod.doTernary(
+    yes, minus, plus
+  )(1, 2)
+  t.equal(-1, result)
+  t.end()
 })
 
-describe('ifThen', () => {
-  it('should call the first function', () => {
-    const result = mod.ifThen(
-      yes, minus
-    )(1, 2)
-    expect(result).toBe(-1)
-  })
-
-  it('should return undefined', () => {
-    const result = mod.ifThen(
-      no, minus
-    )(1, 2)
-    expect(result).toBe()
-  })
+tape('should call the second function', (t) => {
+  const result = mod.doTernary(
+    no, minus, plus
+  )(1, 2)
+  t.equal(3, result)
+  t.end()
 })
 
-test.each([
+tape('should call the first function', (t) => {
+  const result = mod.ifThen(
+    yes, minus
+  )(1, 2)
+  t.equal(-1, result)
+  t.end()
+})
+
+// ifThen
+tape('should return undefined', (t) => {
+  const result = mod.ifThen(
+    no, minus
+  )(1, 2)
+  t.equal(undefined, result)
+  t.end()
+})
+
+;[
   [1, 1, 1, true],
   [1, 0, 1, false],
   [1, 1, 0, false],
   [1, 0, 0, false],
-])('both(%d)(%d, %d) === %s', (a, b, c, expected) => {
-  expect(mod.both(a)(b, c)).toBe(expected)
+].forEach(([a, b, c, expected]) => {
+  tape('both(%d)(%d, %d) === %s', (t) => {
+    t.equal(expected, mod.both(a)(b, c))
+    t.end()
+  })
 })
 
-describe('whitelist', () => {
-  describe('when all values are OK', () => {
-    it('should not throw', () => {
-      const whitelist = mod.createWhitelistChecker(
-        ['a', 'b', 'c'],
-        throws,
-        yes
-      )
-      whitelist('a', 'a', 'c', 'b')
-    })
+// whitelist
+tape('should not throw', (t) => {
+  const whitelist = mod.createWhitelistChecker(
+    ['a', 'b', 'c'],
+    throws,
+    yes
+  )
+  whitelist('a', 'a', 'c', 'b')
+  t.end()
+})
 
-    it('should return value of "good" function', () => {
-      const whitelist = mod.createWhitelistChecker(
-        ['a', 'b', 'c'],
-        throws,
-        (...args) => args
-      )
-      expect(whitelist('a', 'a', 'c', 'b')).toEqual(['a', 'a', 'c', 'b'])
-    })
-  })
+tape('should return value of "good" function', (t) => {
+  const whitelist = mod.createWhitelistChecker(
+    ['a', 'b', 'c'],
+    throws,
+    (...args) => args
+  )
+  t.deepEqual(['a', 'a', 'c', 'b'], whitelist('a', 'a', 'c', 'b'))
+  t.end()
+})
 
-  it('should throw', () => {
-    const whitelist = mod.createWhitelistChecker(['a', 'b'], throws)
-    expect(() => {
-      whitelist('a', 'a', 'horse', 'b')
-    }).toThrow('yoink')
-  })
+tape('should throw', (t) => {
+  const whitelist = mod.createWhitelistChecker(['a', 'b'], throws)
+  try {
+    whitelist('a', 'a', 'horse', 'b')
+  } catch (error) {
+    t.equal('yoink', error.message)
+  }
+  t.end()
 })
